881e5820b88fc0d88b30421b0e677d731fc3a87d
shkero <shkero@users.noreply.github.com>
2023-03-25 17:42:18 +0800

    Update sb.go

--- sb.go
+++ sb.go
@@ -40,7 +40,7 @@ func (s *SingBox) Start(basePath, configPath string) error {
 
 	C.SetBasePath(basePath)
 
-	instance, cancel, err := create(basePath)
+	instance, cancel, err := create(configPath)
 	if err != nil {
 		return err
 	}
